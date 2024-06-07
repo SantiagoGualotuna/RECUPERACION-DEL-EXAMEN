e69590be15d6013a740b84552b41711cc064de8b
Santiago gualotuña <sanexitoxx@gmail.com>
2024-06-07 10:37:34 -0500

    EJERCICIO-PiedraPapelTijera.java

--- src/main/java/com/example/recuperaciondelexamen/EJERCICIO-PiedraPapelTijera.java
+++ src/main/java/com/example/recuperaciondelexamen/EJERCICIO-PiedraPapelTijera.java
@@ -1,4 +1,4 @@
-package com.example.ejerciciopiedrapapeltijera;
+package com.example.ejercicio_2_ppt;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,49 +7,54 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Scanner;
-public class PiedraPapelTijera {
-    public static String calcularGanador(String[][] partidas) {
-        int player1Wins = 0;
-        int player2Wins = 0;
-    
-        for (String[] partida : partidas) {
-            String player1Move = partida[0]; 
-            String player2Move = partida[1];
+public class HelloApplication  {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int Player1 = 0;
+        int Player2 = 0;
+        System.out.println("Welcome to the game");
+        System.out.println("Enter 'r' for rock, 'p' for paper or 's' for scissors.");
+        System.out.println("Enter 'end' to finish the game");
 
-            if (!player1Move.equals(player2Move)) {
-                switch (player1Move) {
-                    case "R":
-                        if (player2Move.equals("S")) player1Wins++;
-                        else player2Wins++; 
-                        break;
-                    case "P":
-                        if (player2Move.equals("R")) player1Wins++; 
-                        else player2Wins++;
-                        break;
-                    case "S":
-                        if (player2Move.equals("P")) player1Wins++; 
-                        else player2Wins++;
-                        break;
-                }
+        while (true) {
+            System.out.print("Player 1: ");
+            String jugada1 = scanner.nextLine();
+
+            if (jugada1.equals("end")) {
+                break;
+            }
+
+            System.out.print("Player 2: ");
+            String jugada2 = scanner.nextLine();
+
+            if (jugada2.equals("end")) {
+                break;
+            }
+
+            if (jugada1.equals(jugada2)) {
+                System.out.println("tie!");
+            } else if ((jugada1.equals("r") && jugada2.equals("s")) ||
+                    (jugada1.equals("p") && jugada2.equals("r")) ||
+                    (jugada1.equals("s") && jugada2.equals("p"))) {
+                System.out.println("Player 1 won!");
+                Player1++;
+            } else {
+                System.out.println("Player 2 won!");
+                Player2++;
             }
         }
-     
-        if (player1Wins > player2Wins) {
-            return "Player 1";
-        } else if (player2Wins > player1Wins) {
-            return "Player 2";
+
+        System.out.println("Player 1 won " + Player1 + " partidas.");
+        System.out.println("Player 2 won " + Player2 + " partidas.");
+
+        if (Player1 > Player2) {
+            System.out.println("¡Player 1 won the game!");
+        } else if (Player1 < Player2) {
+            System.out.println("¡Player 2 won the game!");
         } else {
-            return "Tie";
+            System.out.println("¡player 1 and player 2 tied!");
         }
-    }
-    public static void main(String[] args) {
-        
-        String[][] partidas1 = {
-                {"R", "S"}, 
-                {"S", "R"}, 
-                {"P", "S"}  
-        };
-        System.out.println("Resultado: " + calcularGanador(partidas1)); 
+
+        scanner.close();
     }
 }
-
