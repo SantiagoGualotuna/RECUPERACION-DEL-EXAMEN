92aff2b1912b183bbdd8644aed2e79ebf77c7d88
Santiago gualotuña <sanexitoxx@gmail.com>
2024-06-07 10:25:52 -0500

    EJERCICIO-Robot.java

--- src/main/java/com/example/recuperaciondelexamen/EJERCICIO-Robot.java
+++ src/main/java/com/example/recuperaciondelexamen/EJERCICIO-Robot.java
@@ -1,4 +1,4 @@
-package com.example.ejerciciorobot;
+package org.example.robot;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,43 +6,35 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Scanner;
-public class Robot {
-    // Método para calcular las coordenadas finales del robot
-    public static int[] calcularCoordenadas(int[] pasos) {
-        // Coordenadas iniciales
-        int x = 0;
-        int y = 0;
-        // Dirección inicial: mirando hacia arriba en el eje y
-        // Direcciones: 0 = y positivo, 1 = x negativo, 2 = y negativo, 3 = x positivo
-        int direccion = 0;
-        // Recorremos todos los pasos
-        for (int paso : pasos) {
-            // Actualizamos las coordenadas según la dirección actual
-            switch (direccion) {
-                case 0: // y positivo
-                    y += paso;
-                    break;
-                case 1: // x negativo
-                    x -= paso;
-                    break;
-                case 2: // y negativo
-                    y -= paso;
-                    break;
-                case 3: // x positivo
-                    x += paso;
-                    break;
-            }
-            // Giramos 90 grados en el sentido contrario a las agujas del reloj
-            direccion = (direccion + 1) % 4;
-        }
-        // Devolvemos las coordenadas finales en un arreglo
-        return new int[]{x, y};
-    }
+
+
+public class HelloApplication {
     public static void main(String[] args) {
-        // Caso de prueba 1
-        int[] pasos1 = {10, 5, -2};
-        int[] coordenadas1 = calcularCoordenadas(pasos1);
-        System.out.println("Coordenadas finales: (x: " + coordenadas1[0] + ", y: " + coordenadas1[1] + ")"); 
+        int[]Robot = {10,5,-2};
+        int locationx=0;
+        int locationy=0;
+        boolean X=false;
+        boolean Y=true;
+
+        for( int i = 0 ; i <= Robot.length-1 ; i++){
+            if(i % 2 == 0){
+                if(Y){
+                    locationy =(locationy - Robot[i]) ;
+                    Y=false;
+                }else{
+                    locationy =(locationy + Robot[i]) ;
+                    Y=true;
+                }
+            }else{
+                if(X){
+                    locationx =(locationx - Robot[i]) ;
+                    X=false;
+                }else{
+                    locationx =(locationx + Robot[i]) ;
+                    X=true;
+                }
+            }
+        }
+        System.out.println("x:" + locationx + " y: "+ locationy +"");
     }
 }
